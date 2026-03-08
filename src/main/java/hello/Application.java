f845c2152838ce2fabda474b1c96e3eee446a0d9
bu0256 <carlos.gonzalez.diaz>
2026-03-08 17:04:21 +0100

    make the application executable issue#1

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,9 @@
+package hell;
+import org.springframework.boot.SpringApplication;
+import org.dpringframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
